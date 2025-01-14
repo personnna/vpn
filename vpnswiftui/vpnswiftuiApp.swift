ad2aa1de576476a14a39aafed6cd4a2ee22fabee
personnna <kadyelyer@gmail.com>
2025-01-14 18:30:58 +0100

    Initial Commit

---
+++ vpnswiftui/vpnswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  vpnswiftuiApp.swift
+//  vpnswiftui
+//
+//  Created by ellkaden on 14.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct vpnswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
